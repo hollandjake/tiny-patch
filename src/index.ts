2479eda6fa055ecc90295987f37fedd8e8636277
Jake Holland <contact@hollandjake.com>
2026-07-24 16:46:41 +0100

    feat: now exports helper methods
    - Exposes pointer decode/encode
    - Patch transformation logic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
-import { type ApplyOptions, apply } from "./apply";
-import { type DiffOptions, diff } from "./diff";
-import { type HashFn, hash } from "./hash";
+import { apply } from "./apply";
+import { diff } from "./diff";
+import { hash } from "./hash";
+import { isMaximised, isMinified, maximize, minify } from "./patch";
+import { decodePointer, decodeSegment, encodePointer, encodeSegment } from "./pointer";
 
+export type { ApplyOptions } from "./apply";
+export type { DiffOptions } from "./diff";
+export type { HashFn } from "./hash";
 export type { Maxi, Mini, Op, Patch } from "./patch";
-export { type ApplyOptions, apply, type DiffOptions, diff, type HashFn, hash };
+export {
+    apply,
+    decodePointer,
+    decodeSegment,
+    diff,
+    encodePointer,
+    encodeSegment,
+    hash,
+    isMaximised,
+    isMinified,
+    maximize,
+    minify,
+};
 
-export default { apply, diff, hash };
+export default {
+    apply,
+    decodePointer,
+    decodeSegment,
+    diff,
+    encodePointer,
+    encodeSegment,
+    hash,
+    isMaximised,
+    isMinified,
+    maximize,
+    minify,
+};
